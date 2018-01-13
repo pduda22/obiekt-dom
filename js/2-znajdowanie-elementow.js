88dcd900f952b8abdf97805100a094391fff08ad
Piotr Duda <p.duda_22@wp.pl>
2018-01-13 11:04:20 +0100

    wezly

--- js/2-znajdowanie-elementow.js
+++ js/2-znajdowanie-elementow.js
@@ -1,1 +1,23 @@
 "use strict"
+//pobiera do cosoli po id
+//var parFirst = document.getElementById("parFirst");
+//console.log(parFirst);
+//pobiera po klasie
+
+//szuka po klasie linku
+//var linki = document.getElementsByClassName("superlink");
+//console.log(linki);
+
+//wyswietla wszystkie linki po tagu
+//var linkiPoTagu = document.getElementsByTagName("a");
+//console.log(linkiPoTagu);
+
+//wyswietla po selektorze
+
+//var divPoId = document.querySelector("#parSecond");
+//console.log(divPoId);
+
+//wszystkie linki 
+
+var superlinki = document.querySelectorAll(".superlink");
+console.log(superlinki);
